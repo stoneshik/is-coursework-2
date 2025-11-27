78f40ae4f6226e2e22fbcc01b38ec4b2b8b85604
stoneshik <strebilly@mail.ru>
2025-11-27 14:08:41 +0300

    обновил комментарии

--- check_indexes.sql
+++ check_indexes.sql
@@ -4,39 +4,39 @@
 SET enable_indexscan = off;
 SET enable_bitmapscan = off;
 
--- 1. user_email_index_hash
+-- 1. not_index user_email_index_hash
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT COUNT(*) FROM users WHERE user_email = 'user_50000@example.com';
 
--- 2. user_login_index_hash
+-- 2. not_index user_login_index_hash
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT COUNT(*) FROM users WHERE user_login = 'user_75000';
 
--- 3. order_type_index_hash
+-- 3. not_index order_type_index_hash
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT COUNT(*) FROM orders WHERE order_type = 'print';
 
--- 4. order_status_index_hash
+-- 4. not_index order_status_index_hash
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT COUNT(*) FROM orders WHERE order_status = 'completed';
 
--- 5. vending_point_schedule_day_week_index_hash
+-- 5. not_index vending_point_schedule_day_week_index_hash
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT COUNT(*) FROM vending_point_schedules WHERE vending_point_schedule_day_week = 'monday';
 
--- 6. vending_point_unusual_schedule_date_index_hash
+-- 6. not_index vending_point_unusual_schedule_date_index_hash
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT COUNT(*) FROM vending_point_unusual_schedules WHERE vending_point_unusual_schedule_date = current_date - interval '10 days';
 
--- 7. function_variant_index_hash
+-- 7. not_index function_variant_index_hash
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT COUNT(*) FROM function_variants WHERE function_variant = 'color_print';
 
--- 8. machine_supplies_datetime_index_btree
+-- 8. not_index machine_supplies_datetime_index_btree
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT COUNT(*) FROM machine_supplies WHERE machine_supplies_datetime BETWEEN now() - interval '30 days' AND now();
 
--- 9. machine_condition_datetime_index_btree
+-- 9. not_index machine_condition_datetime_index_btree
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT COUNT(*) FROM machine_conditions WHERE machine_condition_datetime BETWEEN now() - interval '7 days' AND now();
 
@@ -44,38 +44,38 @@ SELECT COUNT(*) FROM machine_conditions WHERE machine_condition_datetime BETWEEN
 SET enable_indexscan = on;
 SET enable_bitmapscan = on;
 
--- 1. user_email_index_hash
+-- 1. index user_email_index_hash
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT COUNT(*) FROM users WHERE user_email = 'user_50000@example.com';
 
--- 2. user_login_index_hash
+-- 2. index user_login_index_hash
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT COUNT(*) FROM users WHERE user_login = 'user_75000';
 
--- 3. order_type_index_hash
+-- 3. index order_type_index_hash
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT COUNT(*) FROM orders WHERE order_type = 'print';
 
--- 4. order_status_index_hash
+-- 4. index order_status_index_hash
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT COUNT(*) FROM orders WHERE order_status = 'completed';
 
--- 5. vending_point_schedule_day_week_index_hash
+-- 5. index vending_point_schedule_day_week_index_hash
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT COUNT(*) FROM vending_point_schedules WHERE vending_point_schedule_day_week = 'monday';
 
--- 6. vending_point_unusual_schedule_date_index_hash
+-- 6. index vending_point_unusual_schedule_date_index_hash
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT COUNT(*) FROM vending_point_unusual_schedules WHERE vending_point_unusual_schedule_date = current_date - interval '10 days';
 
--- 7. function_variant_index_hash
+-- 7. index function_variant_index_hash
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT COUNT(*) FROM function_variants WHERE function_variant = 'color_print';
 
--- 8. machine_supplies_datetime_index_btree
+-- 8. index machine_supplies_datetime_index_btree
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT COUNT(*) FROM machine_supplies WHERE machine_supplies_datetime BETWEEN now() - interval '30 days' AND now();
 
--- 9. machine_condition_datetime_index_btree
+-- 9. index machine_condition_datetime_index_btree
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT COUNT(*) FROM machine_conditions WHERE machine_condition_datetime BETWEEN now() - interval '7 days' AND now();
