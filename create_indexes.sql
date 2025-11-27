9d40ede7927165b322eeaac9dcfdd3445c2f0076
stoneshik <strebilly@mail.ru>
2025-11-27 23:48:15 +0300

    правка названия индекса

--- create_indexes.sql
+++ create_indexes.sql
@@ -5,7 +5,7 @@ CREATE INDEX user_login_index_hash ON users USING hash(user_login);
 CREATE INDEX order_type_index_btree ON orders USING btree(order_type);
 CREATE INDEX order_status_index_btree ON orders USING btree(order_status);
 
-CREATE INDEX vending_point_unusual_schedule_date_hash ON vending_point_unusual_schedules
+CREATE INDEX vending_point_unusual_schedule_date_index_hash ON vending_point_unusual_schedules
     USING hash(vending_point_unusual_schedule_date);
 
 CREATE INDEX function_variant_index_btree ON function_variants USING btree(function_variant);
